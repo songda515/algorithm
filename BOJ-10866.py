4ba19ea425390331c38d54ddcbaa91d928d143d4
Danna <songda515@gmail.com>
2021-02-10 16:13:30 +0900

    [기초 자료구조] BOJ 10866 덱 코드개선

--- BOJ-10866.py
+++ BOJ-10866.py
@@ -1,44 +1,39 @@
 from sys import stdin
-from collections import deque
 input = stdin.readline
 
 n = int(input())
-queue = deque()
+deque = []
 for _ in range(n):
-    cmd = input().strip()
-    if cmd == 'pop_front':
-        if len(queue) > 0:
-            print(queue.popleft())
+    command = input().strip()
+    if 'push_front' in command:
+        deque.insert(0, command.split()[1])
+    elif 'push_back' in command:
+        deque.append(command.split()[1])
+    elif command == 'pop_front':
+        if deque:
+            print(deque.pop(0))
         else:
             print(-1)
-    elif cmd == 'pop_back':
-        if len(queue) > 0:
-            print(queue.pop())
+    elif command == 'pop_back':
+        if deque:
+            print(deque.pop())
         else:
             print(-1)
-    elif cmd == 'size':
-        print(len(queue))
-    elif cmd == 'empty':
-        if len(queue) > 0:
+    elif command == 'size':
+        print(len(deque))
+    elif command == 'empty':
+        if deque:
             print(0)
         else:
             print(1)
-    elif cmd == 'front':
-        if len(queue) > 0:
-            print(queue[0])
+    elif command == 'front':
+        if deque:
+            print(deque[0])
         else:
             print(-1)
-    elif cmd == 'back':
-        if len(queue) > 0:
-            print(queue[-1])
+    elif command == 'back':
+        if deque:
+            print(deque[-1])
         else:
             print(-1)
-    else:
-        cmd = cmd.split()
-        if cmd[0] == 'push_front':
-            queue.appendleft(cmd[1])
-        elif cmd[0] == 'push_back':
-            queue.append(cmd[1])
-            
-    print(queue)
         
