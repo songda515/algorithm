11390a9d30c1c789ccb5e5b03c24caec36138649
Danna <songda515@gmail.com>
2021-02-11 01:49:38 +0900

    [기초 자료구조] BOJ 10866 - deque 라이브러리 이용

--- BOJ-10866.py
+++ BOJ-10866.py
@@ -1,39 +1,40 @@
 from sys import stdin
+from collections import deque
 input = stdin.readline
 
 n = int(input())
-deque = []
+queue = deque()
 for _ in range(n):
     command = input().strip()
     if 'push_front' in command:
-        deque.insert(0, command.split()[1])
+        queue.appendleft(command.split()[1])
     elif 'push_back' in command:
-        deque.append(command.split()[1])
+        queue.append(command.split()[1])
     elif command == 'pop_front':
-        if deque:
-            print(deque.pop(0))
+        if len(queue) > 0:
+            print(queue.popleft())
         else:
             print(-1)
     elif command == 'pop_back':
-        if deque:
-            print(deque.pop())
+        if len(queue) > 0:
+            print(queue.pop())
         else:
             print(-1)
     elif command == 'size':
-        print(len(deque))
+        print(len(queue))
     elif command == 'empty':
-        if deque:
+        if len(queue) > 0:
             print(0)
         else:
             print(1)
     elif command == 'front':
-        if deque:
-            print(deque[0])
+        if len(queue) > 0:
+            print(queue[0])
         else:
             print(-1)
     elif command == 'back':
-        if deque:
-            print(deque[-1])
+        if len(queue) > 0:
+            print(queue[-1])
         else:
             print(-1)
         
