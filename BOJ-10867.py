22950d95b89e3192035919e5436945a55bc09bd5
Danna <songda515@gmail.com>
2021-02-15 03:54:28 +0900

    [정렬] 10867 중복 빼고 정렬하기 - set & sort

--- BOJ-10867.py
+++ BOJ-10867.py
@@ -2,11 +2,7 @@ from sys import stdin
 input = stdin.readline
 
 n = int(input())
-num = list(map(int, input().split()))
-count = [0] * 2003
-for n in num:
-    count[n+1001] += 1
-
-for i, c in enumerate(count):
-    if c > 0:
-        print(i-1001, end=" ") 
+num = set(map(int, input().split()))
+num = list(num)
+num.sort()
+print(" ".join(map(str, num)))
