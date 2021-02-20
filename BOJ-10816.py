fb86c11189fd5b84b3784d358f8c889b524b2be0
Danna <songda515@gmail.com>
2021-02-21 03:36:14 +0900

    [탐색] 10816 숫자 카드 2 - Counter 이용

--- BOJ-10816.py
+++ BOJ-10816.py
@@ -1,20 +1,16 @@
 from sys import stdin
-from bisect import bisect_left, bisect_right
+from collections import Counter
 
 input = stdin.readline
-def count(array, num):
-    left = bisect_left(array, num)
-    right = bisect_right(array, num)
-    return right - left
 
 n = int(input())
 nums = list(map(int, input().split()))
 m = int(input())
 finds = list(map(int, input().split()))
 
-nums.sort()
+count = Counter(nums) # dictionary
 result = []
 for f in finds:
-    result.append(count(nums, f))
+    result.append(count[f])
 
 print(" ".join(map(str, result)))
