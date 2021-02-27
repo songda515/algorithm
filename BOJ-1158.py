b1b3c4267e7a0f7cab63896809ce2c932eb22d87
Danna <songda515@gmail.com>
2021-02-28 02:32:46 +0900

    [기초 자료구조] 1158 요세푸스 문제 - 풀이3

--- BOJ-1158.py
+++ BOJ-1158.py
@@ -7,12 +7,9 @@ n, k = map(int, input().split())
 nums = list(range(1, n+1))
 index = k - 1
 result = []
-while(True):
+for _ in range(n):
+    index = index % len(nums)    
     result.append(nums.pop(index))
-    if not nums:
-        break
     index += k - 1
-    if index >= len(nums):
-        index = index % len(nums)
 
 print("<{}>".format(", ".join(map(str, result))))
