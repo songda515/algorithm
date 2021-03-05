3be94d97ac2ec93fdf84a52814c36cca6544b554
Danna <songda515@gmail.com>
2021-03-05 16:36:39 +0900

    [기초 자료구조] Programmers - printer 풀이2

--- programmers_printer.py
+++ programmers_printer.py
@@ -8,27 +8,29 @@
 3. 그렇지 않으면 J 를 인쇄
 """
 
+from collections import deque
+
 # priorities : 현재 대기목록에 있는 문서의 중요도가 순서대로 담긴 배열
 # location : 내가 인쇄를 요청한 문서가 현재 대기목록의 어떤 위치에 있는지
 # return : 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지
 def solution(priorities, location):
-    length = len(priorities)
-    stack = list(range(0, length))
+    queue = deque(range(len(priorities)))
     count = 0
 
-    while(stack):
+    while(queue):
         # 가장 앞에 있는 문서 J 를 꺼냄
-        j = stack.pop(0)
-        for i in stack:
+        j = queue.popleft()
+        
+        for i in queue:
             if j == i: 
                 pass
             # J 보다 중요도가 높은 게 있다면 J 를 마지막으로
             if priorities[j] < priorities[i]:
-                stack.append(j)
+                queue.append(j)
                 break
         
         # j 가 스택에 없다면 리턴된것
-        if not j in stack:
+        if not j in queue:
             count += 1
             # j 가 찾는 값이라면 반복 종료 
             if j == location:
