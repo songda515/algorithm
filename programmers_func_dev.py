5e38cd307baf7f9ed68688259953b4d9c04887e6
Danna <songda515@gmail.com>
2021-03-06 17:10:43 +0900

    [기초 자료구조] Programmers 기능 개발 - 다른 풀이 참고

--- programmers_func_dev.py
+++ programmers_func_dev.py
@@ -8,31 +8,23 @@ return : 각 배포마다 몇 개의 기능이 배포되는지
 """
 
 def solution(progresses, speeds):
-    count = []
-    distribute = [0] * len(progresses)
-    current = 0
-    while(min(distribute) == 0):
-        for i, progress in enumerate(progresses):
-            if progress >= 100 and distribute[i] == 0:
-                distribute[i] = 1
-            else:
-                progresses[i] += speeds[i]
+    answer = []
+    time = 0
+    count = 0
+    while len(progresses) > 0:
+        print(progresses, speeds, answer, time)
+        if (progresses[0] + time*speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
 
-        if 1 in distribute:        
-            found = distribute.index(1)
-            if current == found:
-                c = 0
-                for i in range(current, len(distribute)):
-                    if distribute[i] == 0:
-                        break # 중간에 배포안된 부분
-                    if distribute[i] == 1:
-                        c += 1
-                        distribute[i] = 2
-                count.append(c)
-                current += c
-
-        print("\n", progresses, speeds, distribute, count)
-    return count
 
 print(solution([93, 30, 55], [1, 30, 5])) # return [2, 1]
 print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])) # return [1, 3, 2]
