7a4cfe917398a015cf69f30dbdf6816765092eb6
Danna <songda515@gmail.com>
2021-03-11 20:48:41 +0900

    [DFS/BFS] BOJ 1260 DFS와 BFS - 코드 개선

--- BOJ-1260.py
+++ BOJ-1260.py
@@ -3,54 +3,45 @@
 # DFS (Depth First Search)
 # DFS 는 stack 자료구조 혹은 재귀함수를 이용한다.
 def dfs(graph, v, visited):
-    # 현재 노드 방문처리
-    visited[v] = True
+    visited[v] = True # 방문처리
     print(v, end=" ")
-    # 인접 노드 반복 
-    for node in graph[v]:
-        # 인접 노드 중 방문하지 않은 노드에 대해 DFS 수행
+    for node in graph[v]: # 인접 노드 반복
         if not visited[node]:
-            dfs(graph, node, visited)
+            dfs(graph, node, visited) # DFS 수행
 
 
 # BFS (Breath First Search)
 # BFS 는 queue 자료구조를 이용한다.
 from collections import deque
 def bfs(graph, start, visited):
+    # 시작 노드 방문처리
     queue = deque([start])
-    # 현재 노드 방문처리
     visited[start] = True
-    # 큐가 빌 때까지 반복
     while(queue):
-        # 큐에서 원소를 하나 뽑기 -> 현재 노드
         v = queue.popleft()
         print(v, end=" ")
-        # 인접 노드 반복
-        for node in graph[v]:
-            # 인접 노드 중 방문하지 않은 노드 queue 에 추가 & 방문처리
+        for node in graph[v]: # 인접 노드 반복
             if not visited[node]:
-                queue.append(node)
-                visited[node] = True
+                queue.append(node) # 스택에 추가
+                visited[node] = True # 방문처리
 
 
 from sys import stdin
 input = stdin.readline
 
 n, m, v = map(int, input().split())
-graph = [0]
-for i in range(n):
-    graph.append([])
+graph = [[] for _ in range(n+1)]
 
 for _ in range(m):
     node1, node2 = list(map(int, input().split()))
     graph[node1].append(node2)
     graph[node2].append(node1)
 
-for i in range(1, n+1):
-    graph[i] = sorted(graph[i])
+for nodes in graph:
+    nodes.sort()
 
-visited = [False] * (n+1)
+visited = [False] * len(graph)
 dfs(graph, v, visited)
 print()
-visited = [False] * (n+1)
+visited = [False] * len(graph)
 bfs(graph, v, visited)
